d7a121110df61d515d583516c45a42bd7cf8fb66
Иван Горбунов <grbnv.vn@gmail.com>
2017-08-01 22:35:16 +0300

    Add pom.xml

---
+++ src/main/java/ru/gorbunov/diaries/DiariesApplication.java
@@ -0,0 +1,12 @@
+package ru.gorbunov.diaries;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiariesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiariesApplication.class, args);
+	}
+}
