b923828f032700e341d319ba0e7bc813337ae2b7
Иван Горбунов <grbnv.vn@gmail.com>
2017-08-13 13:59:56 +0300

    Add note-elements page

--- src/main/java/ru/gorbunov/diaries/controller/NoteElementController.java
+++ src/main/java/ru/gorbunov/diaries/controller/NoteElementController.java
@@ -5,11 +5,10 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import ru.gorbunov.diaries.domain.NoteElement;
 import ru.gorbunov.diaries.repository.NoteElementRepository;
@@ -19,7 +18,7 @@ import ru.gorbunov.diaries.repository.specification.NoteElementSpecification;
  *
  * @author Gorbunov.ia
  */
-@RestController
+@Controller
 @RequestMapping(path = "/notes-elements")
 public class NoteElementController {
     
@@ -35,9 +34,10 @@ public class NoteElementController {
     }
     
     @GetMapping
-    public ResponseEntity<List<NoteElement>> getAllNotes() {
+    public String getAllNotes(ModelMap model) {
         log.debug("REST request to get NotesElements.");
         final List<NoteElement> notesElements = noteElementRepository.findAll(noteElementSpecification.byUser());
-        return new ResponseEntity<>(notesElements, HttpStatus.OK);
+        model.addAttribute("notesElements", notesElements);
+        return "notes-elements";
     }    
 }
