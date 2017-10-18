7efae14da16b2965cab5e8a392f7255c42ed704f
= <=>
2017-10-18 20:55:50 +0300

    Add UserService interface

---
+++ src/main/java/ru/gorbunov/diaries/service/UserServiceImpl.java
@@ -0,0 +1,57 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ru.gorbunov.diaries.service;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.gorbunov.diaries.domain.User;
+
+import ru.gorbunov.diaries.repository.RoleRepository;
+import ru.gorbunov.diaries.repository.UserRepository;
+import ru.gorbunov.diaries.security.SecurityUtils;
+
+/**
+ *
+ * @author Gorbunov.ia
+ */
+@Service
+@Transactional
+public class UserServiceImpl implements UserService {
+    
+    private final Logger log = LoggerFactory.getLogger(UserServiceImpl.class);
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final RoleRepository roleRepository;
+
+    public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder, RoleRepository roleRepository) {
+        this.userRepository = userRepository;
+        this.passwordEncoder = passwordEncoder;
+        this.roleRepository = roleRepository;
+    }    
+    
+    @Transactional(readOnly = true)
+    public User getUserByLogin(String login) {
+        return userRepository.findOneByLogin(login);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUser(Integer id) {
+        return userRepository.findOne(id);
+    }
+
+    @Transactional(readOnly = true)
+    public User getUser() {
+        return userRepository.findOneByLogin(SecurityUtils.getCurrentUserLogin());
+    }    
+    
+}
