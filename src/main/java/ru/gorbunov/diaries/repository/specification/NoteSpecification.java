f741c4a965cbb03609892924eb2af5c23fd79b6d
gorbunov-ia <grbnv.vn@gmail.com>
2018-02-10 14:14:57 +0300

    Delete user service from note specification.

--- src/main/java/ru/gorbunov/diaries/repository/specification/NoteSpecification.java
+++ src/main/java/ru/gorbunov/diaries/repository/specification/NoteSpecification.java
@@ -1,16 +1,10 @@
 package ru.gorbunov.diaries.repository.specification;
 
-import javax.persistence.criteria.CriteriaBuilder;
-import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Predicate;
-import javax.persistence.criteria.Root;
-
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import ru.gorbunov.diaries.domain.Note;
 import ru.gorbunov.diaries.domain.User;
-import ru.gorbunov.diaries.service.UserService;
 
 /**
  * Specification for add condition to the notes into query to db.
@@ -21,35 +15,17 @@ import ru.gorbunov.diaries.service.UserService;
 public final class NoteSpecification {
 
     /**
-     * Service for interaction with user.
-     */
-    private final UserService userService;
-
-    /**
-     * Base constructor.
-     *
-     * @param service service for interaction with user
-     */
-    public NoteSpecification(final UserService service) {
-        this.userService = service;
-    }
-
-    /**
      * Create user condition to the query.
      *
+     * @param user object
      * @return specification for query to the user
      */
-    public Specification<Note> byUser() {
-        return new Specification<Note>() {
-            @Override
-            public Predicate toPredicate(final Root<Note> root, final CriteriaQuery<?> cq, final CriteriaBuilder cb) {
-                User user = userService.getUser();
-
-                if (user != null) {
-                    return cb.equal(root.get("user"), user);
-                } else {
-                    return cb.disjunction();
-                }
+    public Specification<Note> byUser(User user) {
+        return (root, cq, cb) -> {
+            if (user != null) {
+                return cb.equal(root.get("user"), user);
+            } else {
+                return cb.disjunction();
             }
         };
     }
@@ -61,12 +37,7 @@ public final class NoteSpecification {
      * @return      specification for query to the note
      */
     public Specification<Note> byId(final Integer id) {
-        return new Specification<Note>() {
-            @Override
-            public Predicate toPredicate(final Root<Note> root, final CriteriaQuery<?> cq, final CriteriaBuilder cb) {
-                return cb.equal(root.get("id"), id);
-            }
-        };
+        return (root, cq, cb) -> cb.equal(root.get("id"), id);
     }
 
     /**
@@ -77,16 +48,13 @@ public final class NoteSpecification {
      * @return          specification for query to the note
      */
     public Specification<Note> orderBy(final String field, final boolean isDesc) {
-        return new Specification<Note>() {
-            @Override
-            public Predicate toPredicate(final Root<Note> root, final CriteriaQuery<?> cq, final CriteriaBuilder cb) {
-                if (isDesc) {
-                    cq.orderBy(cb.desc(root.get(field)));
-                } else {
-                    cq.orderBy(cb.asc(root.get(field)));
-                }
-                return cb.conjunction();
+        return (root, cq, cb) -> {
+            if (isDesc) {
+                cq.orderBy(cb.desc(root.get(field)));
+            } else {
+                cq.orderBy(cb.asc(root.get(field)));
             }
+            return cb.conjunction();
         };
     }
 
--- src/main/java/ru/gorbunov/diaries/service/NoteServiceImpl.java
+++ src/main/java/ru/gorbunov/diaries/service/NoteServiceImpl.java
@@ -2,10 +2,13 @@ package ru.gorbunov.diaries.service;
 
 import org.springframework.data.jpa.domain.Specifications;
 import org.springframework.stereotype.Service;
+
 import ru.gorbunov.diaries.domain.Note;
+import ru.gorbunov.diaries.domain.User;
 import ru.gorbunov.diaries.repository.NoteRepository;
 import ru.gorbunov.diaries.repository.specification.NoteSpecification;
 
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -27,26 +30,42 @@ public class NoteServiceImpl implements NoteService {
     private final NoteSpecification noteSpecification;
 
     /**
+     * Service for interaction with user.
+     */
+    private final UserService userService;
+
+    /**
      * Base constructor.
      *
-     * @param noteRepository     repository for crud operation with db
-     * @param noteSpecification  specification for add condition into query to db
+     * @param noteRepository    repository for crud operation with db
+     * @param noteSpecification specification for add condition into query to db
+     * @param userService       service for interaction with user
      */
     public NoteServiceImpl(final NoteRepository noteRepository,
-                           final NoteSpecification noteSpecification) {
+                           final NoteSpecification noteSpecification,
+                           final UserService userService) {
         this.noteRepository = noteRepository;
         this.noteSpecification = noteSpecification;
+        this.userService = userService;
     }
 
     @Override
     public List<Note> getUserNotesWithSort(String field, boolean isDesc) {
-        return noteRepository.findAll(Specifications.where(noteSpecification.byUser())
+        final User user = userService.getUser();
+        if (user == null) {
+            return Collections.emptyList();
+        }
+        return noteRepository.findAll(Specifications.where(noteSpecification.byUser(user))
                 .and(noteSpecification.orderBy(field, isDesc)));
     }
 
     @Override
     public Note getUserNoteById(Integer noteId) {
-        return noteRepository.findOne(Specifications.where(noteSpecification.byUser())
+        final User user = userService.getUser();
+        if (user == null) {
+            return null;
+        }
+        return noteRepository.findOne(Specifications.where(noteSpecification.byUser(user))
                 .and(noteSpecification.byId(noteId)));
     }
 }
