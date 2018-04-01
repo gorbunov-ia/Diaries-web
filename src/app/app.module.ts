70b0c4d924970f28ba4953f3615a434a16ab2084
gorbunov-ia <Grbnv.vn@gmail.com>
2018-04-01 15:06:17 +0300

    Add note element service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,16 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { AppComponent } from './app.component';
-import { FooterComponent } from './footer/footer.component';
-import { HeaderComponent } from './header/header.component';
-import { HomeComponent } from './home/home.component';
-import { AuthComponent } from './auth/auth.component';
-
 import { AuthService } from './auth.service';
 import { NoteService } from './note.service';
-import { NoteComponent } from './note/note.component';
+import { NoteElementService } from './note-element.service';
+
 import { AppRoutingModule } from './/app-routing.module';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { AuthComponent } from './auth/auth.component';
+import { NoteComponent } from './note/note.component';
 
 @NgModule({
   declarations: [
